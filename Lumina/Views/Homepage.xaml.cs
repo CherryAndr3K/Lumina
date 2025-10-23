985e34d41c8069c594cb9f716c872e727dad9fac
CherryAndr3k <andrekarim2005@gmail.com>
2025-10-23 02:05:52 -0600

    Se hizo la barra principal y crearon las paginas de libros, musica y peliculas, se modifico la pagina de login

--- Lumina/Views/Homepage.xaml.cs
+++ Lumina/Views/Homepage.xaml.cs
@@ -40,6 +40,37 @@ namespace Lumina.Views
         }
 
 
+        // Permite arrastrar la ventana al hacer click en la barra superior
+        private void TopBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        // Minimizar
+        private void BtnMin_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        // Maximizar / Restaurar
+        private void BtnMax_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        // Cerrar
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
 
     }
 }
---
+++ Lumina/Views/Musica.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lumina.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Musica.xaml
+    /// </summary>
+    public partial class Musica : Window
+    {
+        public Musica()
+        {
+            InitializeComponent();
+        }
+    }
+}
