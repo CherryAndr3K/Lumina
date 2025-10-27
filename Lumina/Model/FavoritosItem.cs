5f67cfaee8b139f2af418ca3defa85fa646fa17a
K-CELESTE\cherp <karch.per@gmail.com>
2025-10-26 20:38:27 -0600

    View Favoritos, creacion de la carpeta Services y archivo FavoritosItem

---
+++ Lumina/Model/FavoritosItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumina.Model
+{
+    public enum MediaType { Book, Movie, Music }
+
+    public class FavoriteItem
+    {
+        public string Title { get; set; } = "";
+        public MediaType Type { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
+
